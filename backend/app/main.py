b07939fe3688bee40e22cb8bdcfb4853854c5713
ubay-cmd <muhamadubaydillah28@gmail.com>
2026-07-20 00:21:37 +0700

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+from .status import router as status_router
 from .highlight import router as highlight_router
 from fastapi import FastAPI
 from .upload import router as upload_router
@@ -5,7 +6,7 @@ from .upload import router as upload_router
 app = FastAPI(
     title="ClipAI V3"
 )
-
+app.include_router(status_router)
 app.include_router(upload_router)
 
 @app.get("/")
