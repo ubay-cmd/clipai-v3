a26c9fb9beb6f6672421c627a613ccc43478be21
ubay-cmd <muhamadubaydillah28@gmail.com>
2026-07-20 00:20:26 +0700

    Update upload.py

--- backend/app/upload.py
+++ backend/app/upload.py
@@ -1,26 +1,33 @@
 from fastapi import APIRouter, UploadFile, File
+from .project import create_project_id
+from .database import projects
 import os
 
 router = APIRouter()
 
 UPLOAD_DIR = "uploads"
-
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 @router.post("/upload")
 async def upload_video(
     file: UploadFile = File(...)
 ):
+    project_id = create_project_id()
+
     filepath = os.path.join(
         UPLOAD_DIR,
-        file.filename
+        f"{project_id}_{file.filename}"
     )
 
     with open(filepath, "wb") as f:
         content = await file.read()
         f.write(content)
 
-    return {
+    projects[project_id] = {
+        "id": project_id,
         "filename": file.filename,
+        "filepath": filepath,
         "status": "uploaded"
     }
+
+    return projects[project_id]
